b773ce25ac39f3f5209f8e3361e7ea53ec2da219
Rahul <rahulajmani910@gmail.com>
2021-10-18 12:14:21 +0530

    restructuring reducers and using them

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,38 @@
 import classes from './Header.module.css';
 
+import { useSelector, useDispatch } from 'react-redux'
+import { authActions } from '../store/auth';
+
 const Header = () => {
+
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
+
+  const loginHandler = () => {
+    dispatch(authActions.login())
+  }
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout())
+  }
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
         <ul>
-          <li>
+          {isAuthenticated && <li>
             <a href='/'>My Products</a>
-          </li>
-          <li>
+          </li>}
+          {isAuthenticated && <li>
             <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          </li>}
+          {isAuthenticated && <li>
+            <button onClick={logoutHandler}>Logout</button>
+          </li>}
+          {!isAuthenticated && <li>
+            <button onClick={loginHandler}>Login</button>
+          </li>}
         </ul>
       </nav>
     </header>
